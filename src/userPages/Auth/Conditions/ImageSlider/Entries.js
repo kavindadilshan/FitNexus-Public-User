4a1c22bba86076fd1bc0085460faea423244e435
kavindadilshan <kavindadilshan97@gmail.com>
2024-01-07 09:52:15 +0530

    slider content creation

---
+++ src/userPages/Auth/Conditions/ImageSlider/Entries.js
@@ -0,0 +1,27 @@
+import IMG from '../../../../assets/Auth/Artboard1.png';
+import IMG2 from '../../../../assets/Auth/Artboard2.png';
+import IMG3 from '../../../../assets/Auth/Artboard3.png';
+import IMG4 from '../../../../assets/Auth/Artboard4.png';
+export const ENTRIES1 = [
+    {
+        title: 'Digital Fitness Marketplace',
+        subtitle: 'Access gyms, fitness classes & many more fitness services in one place',
+        illustration: IMG
+    },
+    {
+        title: 'Find the Best around your area ',
+        subtitle: 'Explore the best fitness services around you',
+        illustration: IMG2
+    },
+    {
+        title: 'Guaranteed Best Price',
+        subtitle: 'Get the best price for your purchases up to 80% OFF',
+        illustration: IMG3
+    },
+    {
+        title: 'One-stop App for all Your Fitness Needs',
+        subtitle: 'You can find everything related to your fitness goals in one place full of varieties to choose from.',
+        illustration: IMG4
+    },
+
+];
