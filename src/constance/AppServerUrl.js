235bced76c23ed822d6c3b7a53f6cd1d20c0065d
kavindadilshan <kavindadilshan97@gmail.com>
2024-01-07 09:08:17 +0530

    server related constance creation

---
+++ src/constance/AppServerUrl.js
@@ -0,0 +1,9 @@
+export const ServerUrl = {
+    url: 'https://fitzky.ceyentra.lk/v1/',
+    prod_url: ''
+};
+
+export const Domains = {
+    dev: {url: 'https://fitzky.ceyentra.lk', sha: 'sha256/cui1eHPQKXwTtvNCLPOvbcIrmKfXFOGf3VkZEHOQaAo='},
+    prod: {url: '', sha: ''}
+}
