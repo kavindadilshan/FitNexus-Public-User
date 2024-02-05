25612d01125f42e739734856d65c26ff6ea2fb3b
kavindadilshan <kavindadilshan97@gmail.com>
2024-02-06 00:31:27 +0530

    added constance file

---
+++ src/constance/Const.js
@@ -0,0 +1,10 @@
+export const GROUP_CLASS_ONLY = true;  // as false and please remove condition on online class memberships & profile (my subscription) && (session profile & business profile component)
+export const ONLINE_MEMBERSHIP_VISIBLE = false;
+export const PACKAGE_NUMBER = 0;
+
+export const SINGLE_SESSION_PAYMENT = false;
+
+export const IS_STRIPE_ENABLED = false;
+
+export const PAYHERE_SANDBOX_STATUS = true;
+export const PAYHERE_MERCHANT_ID = "1223358"
